53c9cc9c5139f8555a33c0da5e1dc1a2106e7519
“sharon1999” <sharonantony51@gmail.com>
2026-03-31 23:34:13 +0530

    Add Heroui styles and button component to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@heroui/react";
 import Image from "next/image";
+import { CgSmileMouthOpen } from "react-icons/cg";
 
 export default function Home() {
-  return <div className="text-3xl">Hello</div>;
+  return <div className="text-3xl">Hello
+  <Button type="button"><CgSmileMouthOpen />Primary Button</Button>
+  </div>;
 }
